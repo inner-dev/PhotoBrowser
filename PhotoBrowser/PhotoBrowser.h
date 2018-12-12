ce7636bc15689a5183ed13517a4aa3e3f09f4568
zfmai <waltcow@gmail.com>
2018-12-12 09:46:46 +0800

    Initial Commit

---
+++ PhotoBrowser/PhotoBrowser.h
@@ -0,0 +1,19 @@
+//
+//  PhotoBrowser.h
+//  PhotoBrowser
+//
+//  Created by maizifeng on 2018/12/12.
+//  Copyright © 2018 maizifeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PhotoBrowser.
+FOUNDATION_EXPORT double PhotoBrowserVersionNumber;
+
+//! Project version string for PhotoBrowser.
+FOUNDATION_EXPORT const unsigned char PhotoBrowserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PhotoBrowser/PublicHeader.h>
+
+
